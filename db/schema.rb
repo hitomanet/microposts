73d59759ca9f855e55baeecce3da60225049175c
hitomanet <hitomane.ngs@gmail.com>
2015-11-18 13:46:06 +0000

    profile5

--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20151108053741) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
   add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "users", force: :cascade do |t|
